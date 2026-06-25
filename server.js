ff5525fd9416d0e30510266bd300fcda0fd3a95d
asyanomi <tasyaanjelika53@gmail.com>
2026-06-25 13:58:19 +0700

    fix railway crash

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log("Running on port " + PORT);
+});
 // Serve file static
 
 
